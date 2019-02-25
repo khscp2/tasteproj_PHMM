573a15e9093dc0c1d98eeb67183a6819db911b9e
Pierce <spammermuch@gmail.com>
2019-02-25 12:11:21 -0500

    Change to catering, handler.js

--- components/handler.js
+++ components/handler.js
@@ -1,20 +1,46 @@
+window.onload = (e) => {
+    if(document.getElementById('order-list')) {
+        const rootElement = document.getElementById('order-list');
+        const orders = getOrder();
+        if(orders[0] === "" || orders === undefined) return;
+        orders.forEach(order => {
+            if(order === null) return;
+
+            const li =  document.createElement('li');
+            const a = document.createElement('a')
+            
+            a.innerText = order;
+            li.appendChild(a);
+
+            rootElement.parentNode.insertBefore(li, rootElement.nextSibling);
+        });
+    }
+}
+
 function addItem (item) {
     const order = window.localStorage.getItem('order')
     let items = [];
-
     if(order) items = JSON.parse(order);
-
     items.push(item)
-    
     window.localStorage.setItem('order', JSON.stringify(items));
+    location.reload(); 
 }
 
-function removeItem(item) {
+function removeItem (item) {
     const order = JSON.parse(window.localStorage.getItem('order'));
 
     for(let _item in order) {
-        if(order[_item] === item) order.splice(0, _item)
+        if(order[_item] === item) delete order[_item]
     }
     window.localStorage.setItem('order', JSON.stringify(order));
+    location.reload();    
 }
 
+function getOrder () {    
+    return(JSON.parse(window.localStorage.getItem('order')) || [])
+}
+
+function clearOrder () {
+    window.localStorage.removeItem('order')
+    location.reload();
+}
